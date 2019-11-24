6469632aa3fc12053cefa44b0d9dfddf26ad5510
tylermorrisford <tylerford@gmail.com>
2019-11-24 13:35:15 -0700

    hide nav overflow

---
+++ rental/cottages.js
@@ -0,0 +1,69 @@
+export default data = [
+    {
+        cottageName: "Cedar",
+        cottageLocation: "Leland",
+        cottagePerNight: 719,
+        cottagePerWeek: 5000,
+        cottageSleeps: 8,
+        cottageSlug: "Magical Leland summer home sleeps 8",
+        cottageDescription: "Cedarwood is an enchanting and unique Leland summer home sided in Cedar Shake and trimmed in white and green. It boasts 4 bedrooms and 2.5 baths. Beautiful Lake Michigan is just steps from the house and can be viewed from decks and screened in porches on the lakeside of this 2500 square foot residence. Comfortable leather furniture provides comfortable seating in the living room that boasts a fireplace. Art and books can be found throughout this one-of-kind summer living experience. Stroll to Fishtown, enjoy a walk around the marina, bike to the store or enjoy a meal in several nearby restaurants. Grab a cocktail and enjoy the sunset from the screened in porch. This is truly a taste of paradise!",
+        cottageBedrooms: 4,
+        cottageBathrooms: 3,
+        cottageAmenities: [],
+        cottagePets: false
+    },
+    {
+        cottageName: "Holly's House",
+        cottageLocation: "Leland",
+        cottagePerNight: 245,
+        cottagePerWeek: 1750,
+        cottageSleeps: 4,
+        cottageSlug: "Charming cottage in the heart of Leland",
+        cottageDescription: "This charming two bedroom, one bath cottage sits in the heart of Leland. It was built sometime between 1895 and 1904 and is a potential Michigan Historical Site. Completely updated in 2016 and 2017, it boasts all new stainless steel appliances; a gas stove, a side-by-side refrigerator, a dishwasher, and microwave. The first floor laundry has a matching washer and dryer. Both the kitchen and the bathroom have been totally rebuilt. The living room is warmed by a cozy fireplace and features a book case wall. In the backyard, a separate out building houses a well lit screened in porch with comfortable furniture and a table for dinner for eight. An outdoor grill and fire pit add the finishing touches to a fabulous vacation get-a-way! This well appointed historic home has exceptionally large lot. It is two blocks to downtown Leland. This is the perfect vacation location for two couples or a family of four.",
+        cottageBedrooms: 2,
+        cottageBathrooms: 1,
+        cottageAmenities: [],
+        cottagePets: false
+    },
+    {
+        cottageName: "Maria's Sunset Shore",
+        cottageLocation: "Lake Leelanau",
+        cottagePerNight: 464,
+        cottagePerWeek: 3200,
+        cottageSleeps: 8,
+        cottageSlug: "Your own private beach on South Lake Leelanau",
+        cottageDescription: "Located 25 minutes from the Sleeping Bear Sand Dunes and National Lake Shore Park that was voted The Most Beautiful Place in America in 2011, this summer home is roomy, clean, comfortable, well equipped, well maintained and unique. A second floor balcony surrounds the first floor on three sides creating a two-story great room with a beautiful stone wall fireplace. The house is designed so that the lake is visible from most of the first floor rooms; the living and dining rooms, the master bedroom and the kitchen. Rooms on the second floor open to the balcony which also allows great water views. Most of the bedrooms have new beds and mattresses. To check outdoor amenities, just slide open the door wall in the dining room, move to the porch and down to the lawn, then sink tired feet into the sand on the beach before testing the lake water with its firm, sandy bottom. The lake is shallow a good distance out making this a great area for supervising children’s summer fun. \nThere is a dock and a mooring station to which you can moor your boat. It could also be a great place to soak up the warm sun and dangle toes in the water. \nLazy days in the sun, swimming, fishing, and boating move to evenings by the fire watching the sunset. This is the perfect setting to enjoy family time, to relax, to laugh and to play together. Secluded on a private road, you are centrally located to explore the many attractions that surround you. Create your summer memories at Maria’s Sunset Shore.",
+        cottageBedrooms: 4,
+        cottageBathrooms: 2,
+        cottageAmenities: [],
+        cottagePets: false
+    },
+    {
+        cottageName: "",
+        cottageLocation: "",
+        cottagePerNight: 1,
+        cottagePerWeek: 2000,
+        cottageSleeps: 5,
+        cottageSlug: "a short description of the property",
+        cottageDescription: "a longer description of the property",
+        cottageBedrooms: 2,
+        cottageBathrooms: 2,
+        cottageAmenities: [],
+        cottagePets: false
+    },
+    {
+        cottageName: "",
+        cottageLocation: "",
+        cottagePerNight: 1,
+        cottagePerWeek: 2000,
+        cottageSleeps: 5,
+        cottageSlug: "a short description of the property",
+        cottageDescription: "a longer description of the property",
+        cottageBedrooms: 2,
+        cottageBathrooms: 2,
+        cottageAmenities: [],
+        cottagePets: false
+    },
+]
+
+// hard code check-in and check-out times
