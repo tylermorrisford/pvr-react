1622c15814327ae5109fae14beac81451e5ceb31
tylermorrisford <tylerford@gmail.com>
2019-12-14 13:43:12 -0700

    update server

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const path = require ('path')
+const path = require('path')
 const app = express();
 const PORT = process.env.PORT || 3001;
+require('dotenv').config();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,18 +12,24 @@ app.use(express.json());
 // Serve up static assets (usually on heroku) // ------------------ This will need to be updated before deployment
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("rental/build"));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "rental", "build", "index.html"))
-  })
 }
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "rental", "build", "index.html"))
+// });
+
+// Connect to Mlab
+// const dbuser = process.env.DBUSER;
+// const dbpassword = process.env.DBPASS;
+// const MONGODB_URI = `mongodb://${dbuser}:${dbpassword}@ds353378.mlab.com:53378/heroku_96lg69gp`
+
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/peninsulavacationrentals", {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
 }).then(() => console.log('Db Connected'));
 
 // Start the API server
