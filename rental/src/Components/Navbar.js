007af1c498d71dc4033d623020d837490b9c037d
tylermorrisford <tylerford@gmail.com>
2019-12-16 21:10:54 -0700

    set homepage in package.json

--- rental/src/Components/Navbar.js
+++ rental/src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {NavLink} from "react-router-dom";
+// import {NavLink} from "react-router-dom";
 import "./Navbar.css";
 import { Navbar, Icon, NavItem, Dropdown } from 'react-materialize';
 
